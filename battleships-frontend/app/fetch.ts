165e5cf847895ecfad670f8a2298a161f434ded1
Filip Przygoński <50529336+FilipJQ77@users.noreply.github.com>
2023-06-07 21:58:13 +0200

    Delete battleships-frontend directory

--- battleships-frontend/app/fetch.ts
+++
@@ -1,7 +0,0 @@
-export const API_BASE_URL = "https://localhost:44346";
-
-export const fetchRoute = async (route: string) => {
-  const url = `${API_BASE_URL}/${route}`;
-  const response = await fetch(url);
-  return response.json();
-};
