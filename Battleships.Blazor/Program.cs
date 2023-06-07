3b8b842851d62e2df7c5b9f7d432ae5c5ec49a81
FilipJQ77 <filipprzygonski99@gmail.com>
2023-06-07 20:52:27 +0200

    Everything works

--- Battleships.Blazor/Program.cs
+++ Battleships.Blazor/Program.cs
@@ -12,24 +12,24 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var game = new Game();
 
-// Add AI opponent
+// Add AI opponent TODO add randomness in the future
 game.CreateShips(2, new List<Tile>
 {
-    new Tile(0,0),
-    new Tile(0,1),
-    new Tile(0,2),
-    new Tile(0,3),
-    new Tile(0,4),
+    new(0,0),
+    new(0,1),
+    new(0,2),
+    new(0,3),
+    new(0,4),
     
-    new Tile(3,7),
-    new Tile(4,7),
-    new Tile(5,7),
-    new Tile(6,7),
+    new(3,7),
+    new(4,7),
+    new(5,7),
+    new(6,7),
     
-    new Tile(9,6),
-    new Tile(9,7),
-    new Tile(9,8),
-    new Tile(9,9),
+    new(9,6),
+    new(9,7),
+    new(9,8),
+    new(9,9),
     
 });
 
